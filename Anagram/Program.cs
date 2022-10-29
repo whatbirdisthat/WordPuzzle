6ecc2c27f99572629ce0bdaa084726bf02b2452b
🐦 <95787+whatbirdisthat@users.noreply.github.com>
2022-10-29 13:01:55 +1100

    there is a console application for finding anagrams

---
+++ Anagram/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using WordPuzzle.Lib;
+using WordPuzzle.Lib.Load;
+using WordPuzzle.Lib.Query;
+using WordPuzzle.Lib.Repository;
+using static System.String;
+
+if (args.Length > 0)
+{
+    var sourceWord = args[0];
+    var theRepository = new WordModelRepository<InlineMethod, ProperSubset>();
+    var results = sourceWord.Anagrams(theRepository);
+    Console.WriteLine(
+        $"""
+--- Anagrams for '{ sourceWord}' ---
+{ Join(Environment.NewLine, results)}  
+"""
+    );
+}
+else
+{
+    Console.WriteLine($"""
+Usage: { typeof(Program).Assembly.GetName().Name} <word>
+"""
+    );
+}
